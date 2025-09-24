4b7b1a247e565379969fb8a90294320724415c02
rickegss <ricksilvagomes1@gmail.com>
2025-09-24 09:15:53 -0300

    Versão final

--- src/Interface.java
+++ src/Interface.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 public class Interface {
     int acertos = 0;
     int erros = 0;
@@ -8,46 +6,48 @@ public class Interface {
         System.out.println(" ╔═══════════════════════════════╗");
         System.out.println(" ║ ☄ Questionário Astronômico   ║");
         System.out.println(" ╚═══════════════════════════════╝");
+        System.out.println("Centro Universitário Alfredo Nasser - UNIFAN");
+        System.out.println("Prof: Brenno Pimenta - Algoritmos e Programação II");
         System.out.println("Autor: Ricardo Gomes da Silva");
         System.out.println("Feito em java");
-        System.out.println("---------------------------------------------------------------------");
+        System.out.println("------------------------------------------------------------------------------------------------");
         System.out.println(" ");
     }
 
-    int numero_questao = 1;
-    public void formataQuestao(Questao[] questoes, Interface quiz) {
+
+    public void formataQuestao(Questao[] questoes) {
+        int numero_questao = 1;
         for (Questao questao : questoes) {
             questao.escrevaQuestao(numero_questao);
             String resp = questao.leiaResposta();
             boolean resultado = questao.isCorreta(resp);
-            quiz.contaAcertos(resultado);
-            System.out.println("---------------------------------------------------------------------");
+            contaAcertos(resultado);
+            System.out.println("------------------------------------------------------------------------------------------------");
             numero_questao++;
         }
     }
 
-    public double[] contaAcertos(boolean resultado) {
+    public void contaAcertos(boolean resultado) {
         if (resultado) {
-            acertos ++;
+            acertos++;
         } else {
             erros++;
         }
-        return new double[]{acertos, erros};
     }
 
     public void exibeEstats(Questao[] questoes) {
         int total = questoes.length;
         double porcentagem = ((double) acertos / total ) * 100;
 
-        System.out.println(" ---------- ✰ Fim do Questionário! ---------- ");
+        System.out.println(" ----------------------------  ✰ Fim do Questionário! ---------------------------- ");
         System.out.println(" ");
-        System.out.println("╔══════════════════════════════════════════╗");
-        System.out.println("║ \uD83D\uDC7D Estatísticas:                         ║");
-        System.out.println("║                                          ║");
-        System.out.printf("║ \uD83D\uDCCA Porcentagem de sucesso: %.1f%%         ║\n", porcentagem);
-        System.out.printf("║ ✔ Acertos: %d                            ║\n", acertos);
-        System.out.printf("║ ❌ Erros: %d                             ║\n", erros);
-        System.out.println("╚══════════════════════════════════════════╝");
+        System.out.println("╔═══════════════════════════════════════════╗");
+        System.out.println("║ \uD83D\uDC7D Estatísticas:                          ║");
+        System.out.println("║                                           ║");
+        System.out.printf("║ \uD83D\uDCCA Porcentagem de sucesso: %.1f%%          ║\n", porcentagem);
+        System.out.printf("║ ✔ Acertos: %d                              ║\n", acertos);
+        System.out.printf("║ ❌ Erros: %d                              ║\n", erros);
+        System.out.println("╚═══════════════════════════════════════════╝");
         System.out.println(" ");
         System.out.println("Obrigado por responder ✍! Fique a vontade para tentar novamente.");
     }
